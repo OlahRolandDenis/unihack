a012fdfca3f42d9d0cd6bf34dd5c3441961c0444
Katty Lazar <katy.lazar05@gmail.com>
2022-11-26 20:03:27 +0200

    get position of user

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 
 const Home = () => {
@@ -13,15 +13,57 @@ const Home = () => {
   )
 }
 
+const lat = 45.7663994;
+const lng = 21.1814022;
+
 function Map() {
+    const [ position, setPosition ] = useState({
+        lat: null, 
+        long: null
+    });
+
+    useEffect( () => {
+        const resp = getLocation();
+
+        setPosition(...resp, {
+            lat: resp.coords.latitude,
+            long: resp.coords.longitude
+        })
+        
+
+        console.log(position);
+    }, []);
+
+
+    const successCallback = (position) => {
+        console.log(position);
+      };
+      
+      const errorCallback = (error) => {
+        console.log(error);
+      };
+      
+      function getLocation() {
+          navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+          console.log('trruu')
+      }
+
+      getLocation();
+
     return (
         <GoogleMap
-            zoom={50}
-            center={{lat: 44, lng: 77}}
+            zoom={30}
+            center={{lat: lat, lng: lng}}
             mapContainerClassName="map-container"
+                onClick={ () => getLocation}
         >
-            <Marker 
-                position={{ lat: 10, lng: 10 }}
+            <Marker
+
+                position={{ lat: lat, lng: lng }}
+            />
+            <Marker
+onClick={ () => console.log('adsack')}
+                position={{ lat: lat + 0.02, lng: lng + 0.02 }}
             />
         </GoogleMap>
     )
