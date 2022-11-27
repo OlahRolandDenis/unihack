65760b3b12f9370786a91bd13dfb5ff1186fd9fa
unknown <olah.roland.denid@gmail.com>
2022-11-27 10:13:33 +0200

    signup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
+import './SignUp.css';
 import Home from './Home';
 
 
--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Home from './Home';
+import SignUp from './SignUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SignUp />
   </React.StrictMode>
 );
 
